20892a4a2cd1c8ca50400d03c4a132f7494c36a3
bit-barron <1bit.baron@gmail.com>
2024-05-28 19:42:26 +0200

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/elements/Button";
 import { Input } from "@/components/elements/Input";
 import { useState } from "react";
 import { AiOutlineEnter } from "react-icons/ai";
-import { FaRegMessage } from "react-icons/fa6";
 import { exampleMessages } from "@/lib/utils";
 
 export default function Home() {
@@ -16,6 +15,24 @@ export default function Home() {
     <div>
       <div className="flex justify-center h-screen items-end bg-white">
         <div className="space-y-4 border-t px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
+          <div className="mx-auto sm:max-w-2xl sm:px-4">
+            <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
+              {messages.length === 0 &&
+                exampleMessages.map((example, index) => (
+                  <div
+                    key={example.heading}
+                    className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
+                      index > 1 && "hidden md:block"
+                    }`}
+                  >
+                    <div className="text-sm font-semibold">{example.heading}</div>
+                    <div className="text-sm text-zinc-600">
+                      {example.subheading}
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
           <form>
             <div className="relative flex border">
               <Input
